fd1fd5c99d316ab74bc58a5a54971fb32f932033
KayongoYongo <sknsammie10@gmail.com>
2025-03-24 21:16:32 +0300

    functions, parameters and arguments

--- 0x00_JavaScript_Refresher/JavaScript_Refresher/assets/scripts/app.js
+++ 0x00_JavaScript_Refresher/JavaScript_Refresher/assets/scripts/app.js
@@ -1,6 +1,28 @@
 // import { apiKey } from "./util";
-import abc from "./util";
+//import abc from "./util";
 //import { apiKey,abc,ghi } from "./util";
-import * as util from "./util";
+//import * as util from "./util";
 
-console.log(util.apiKey)
+//console.log(util.apiKey)
+
+// Old way of writing a function
+function greet () {
+    console.log("Hello")
+}
+// The above function is defined but not executed.
+greet();
+
+// Adding parameters
+function greetUser(username, message) {
+    console.log("Hello" + " " + username + " " + message);
+}
+
+// Samuel and whats' up are arguments
+greetUser("Samuel", "What's up?");
+
+// Storing a function in a variable
+const sayGoodbye = function(name) {
+    console.log("Goodbye "+ name);
+}
+
+sayGoodbye("Samuel");
