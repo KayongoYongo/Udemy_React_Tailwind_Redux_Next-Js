42c8075de5db51866e59ca2d0b66c20105f4c8ce
KayongoYongo <sknsammie10@gmail.com>
2025-03-25 10:03:02 +0300

    Getters and setters

--- 0x00_JavaScript_Refresher/JavaScript_Refresher/assets/scripts/app.js
+++ 0x00_JavaScript_Refresher/JavaScript_Refresher/assets/scripts/app.js
@@ -37,3 +37,48 @@ export default (userName, message) => {
 const add = (a, b) => a + b;
 
 console.log(add(5,6));
+
+// Objects in Js
+
+const person = {
+    firstName: "John",
+    secondName: "Doe",
+    age: 30,
+    fullname: function() {
+        return "Hello my name is " + this.firstName + " " + this.secondName + "." + " I am "+ this.age + " years.";
+    }
+}
+
+// Elements of an object can be accessed through dot notation
+console.log(person.fullname());
+
+const human = new Object();
+human.firstName = "Jane";
+human.lastName = "Doe";
+human.age = 25;
+human.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
+
+console.log(human.fullName()); // Output: Jane Doe
+
+// Getters and setters
+class Student { 
+    constructor(name, age) { 
+      this._name = name; 
+      this._age = age; 
+    } 
+   
+    get name() { 
+      return this._name; 
+    } 
+    set name(newName) { 
+      this._name = newName; 
+    }
+  } 
+
+   
+  const student = new Student("Emma", 22); 
+  console.log(student.name); // Output: Emma 
+  student.name = "Olivia"; 
+  console.log(student.name); // Output: Olivia
