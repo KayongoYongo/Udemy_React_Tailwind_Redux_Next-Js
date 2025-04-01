59f617cddd336575fbc5c8602ada05d03164bb84
KayongoYongo <sknsammie10@gmail.com>
2025-04-01 20:14:43 +0300

    Array and array methods

--- 0x00_JavaScript_Refresher/JavaScript_Refresher/assets/scripts/app.js
+++ 0x00_JavaScript_Refresher/JavaScript_Refresher/assets/scripts/app.js
@@ -10,7 +10,7 @@ function greet () {
     console.log("Hello")
 }
 // The above function is defined but not executed.
-greet();
+// greet();
 
 // Adding parameters
 function greetUser(username, message) {
@@ -18,14 +18,14 @@ function greetUser(username, message) {
 }
 
 // Samuel and whats' up are arguments
-greetUser("Samuel", "What's up?");
+// greetUser("Samuel", "What's up?");
 
 // Storing a function in a variable
 const sayGoodbye = function(name) {
     console.log("Goodbye "+ name);
 }
 
-sayGoodbye("Samuel");
+// sayGoodbye("Samuel");
 
 // Arrow functions
 export default (userName, message) => {
@@ -36,7 +36,7 @@ export default (userName, message) => {
 // Arrow functions
 const add = (a, b) => a + b;
 
-console.log(add(5,6));
+// console.log(add(5,6));
 
 // Objects in Js
 
@@ -50,7 +50,7 @@ const person = {
 }
 
 // Elements of an object can be accessed through dot notation
-console.log(person.fullname());
+// console.log(person.fullname());
 
 const human = new Object();
 human.firstName = "Jane";
@@ -60,7 +60,7 @@ human.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
-console.log(human.fullName()); // Output: Jane Doe
+// console.log(human.fullName()); // Output: Jane Doe
 
 // Getters and setters
 class Student { 
@@ -79,6 +79,33 @@ class Student {
 
    
   const student = new Student("Emma", 22); 
-  console.log(student.name); // Output: Emma 
+  // console.log(student.name); // Output: Emma 
   student.name = "Olivia"; 
-  console.log(student.name); // Output: Olivia
+  // console.log(student.name); // Output: Olivia
+
+// Arrays and array methods
+// Creating an array
+const hobbies = ["Cooking", "Racing", "Dancing", "Cleaning"]
+
+// Accessing elements in an array
+
+// console.log(hobbies[2])
+
+// Array methods
+// This adds an element to the end of the array
+hobbies.push("Eating");
+
+// Find the first occurence of a value
+const index = hobbies.findIndex((item) => item === "Dancing");
+
+console.log(index);
+
+// Map method - ALlows to transform an item into another item
+const editedHobbies = hobbies.map((item) => item + "!");
+
+// console.log(editedHobbies);
+
+// You can also convert the items into JavaScript objects
+const objectHobbies = hobbies.map((item) => ({hobby: item}));
+
+console.log(objectHobbies);
